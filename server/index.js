f0b4c6e37299c18d221ee6e4a2521df151c50352
Abhishek-A <kua77663@gmail.com>
2023-10-05 10:51:43 +0530

    created  textfile

--- server/index.js
+++ server/index.js
@@ -1,30 +1,15 @@
-const http = require('http');
-const fs = require("fs");
 
-const url = require("url");
+const http = require("http");
+ const fs = require("fs");
+
 const myServer = http.createServer((req, res) => {
-    if(req.url === "/favicon.ico") return res.end();
-const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
-const myUrl = url.parse(req.url, true);
-console.log(myUrl);
-fs.appendFile("log.txt", log, (err, data) => {
+     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+     fs.appendFile('log.txt', log, (err, data) => {
+        
+       
+        res.end("Hello from server Again !");
+     });
    
-    switch(myUrl.pathname) {
-        case "/":
-        res.end("HomePage");
-        break;
-        case "/about": 
-        const username = myUrl.query.myname;
-        res.end(`hi, ${username}`);
-        break;
-        case "/search" :
-            const search = myUrl.query.search_query;
-            res.end("Here are your result for " + search);
-        default:
-            res.end("404 Not Found");
-    }
 });
 
-});
-
-myServer.listen(8000), () => console.log("server started");
+myServer.listen(8000, () => console.log("server started"));
