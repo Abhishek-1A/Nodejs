c47cd045e5b6e2bdbd542ea703a006ba2c9edd65
Abhishek-A <kua77663@gmail.com>
2023-10-05 12:06:23 +0530

    Handling http Methods

--- server/index.js
+++ server/index.js
@@ -4,17 +4,28 @@ const http = require("http");
  const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+     const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
      const myUrl = url.parse(req.url, true);
-     console.log(myUrl);
+     
      fs.appendFile('log.txt', log, (err, data) => {
        switch(myUrl.pathname) {
-       case "/" : res.end("HomePage"); 
+       case "/" : 
+       if(req.method == "GET") res.end("HomePage");
        break;
        case "/about" :
          const username = myUrl.query.myname;
          res.end(`Hi , ${username}`);
        break;
+       case "/search" :
+         const search = myUrl.query.search_query;
+         res.end("Here are your result for " + search);
+
+         case "/signup":
+            if(req.method === "GET") res.end("This is a Signup form");
+            else if(req.method === "POST") {
+               // DB query
+               res.end("Success")
+            }
        default: 
        res.end("404 not found");
        }
